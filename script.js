7309775428e5f69d4dd2ce6f8c6d7edf9bd61365
Ayushi Patwal <ayushipatwal03@gmail.com>
2026-03-14 12:51:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+
+
+function updateTimer() {
+  const now = new Date();
+  const time = now.getDate() + "/" + (now.getMonth()+1) + "/" + now.getFullYear() +
+               " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+  document.getElementById("timer").textContent = time;
+}
+
+setInterval(updateTimer, 1000);
+
+
+
+
+
